d89ced959d6970a3333c283f1f83d0843f15dfaf
Sedanur Akkoyun <akkoyunsedanur@gmail.com>
2022-12-31 20:53:05 +0300

    First Spring Web Application Creation

---
+++ src/main/java/com/garanti/FirstSpringWeb/model/Person.java
@@ -0,0 +1,15 @@
+package com.garanti.FirstSpringWeb.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Person {
+    private int yas;
+    private String isim;
+
+}

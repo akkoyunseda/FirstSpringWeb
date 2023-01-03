45f1f309aa5b5d17bb6ed68c852aadb57d21e894
Sedanur Akkoyun <akkoyunsedanur@gmail.com>
2023-01-03 21:04:37 +0300

    Repo and Controller Updates

--- src/main/java/com/garanti/FirstSpringWeb/config/BeanFactory.java
+++ src/main/java/com/garanti/FirstSpringWeb/config/BeanFactory.java
@@ -1,20 +1,38 @@
 package com.garanti.FirstSpringWeb.config;
 
+import com.garanti.FirstSpringWeb.Constants;
 import com.garanti.FirstSpringWeb.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
 
 @Component
 public class BeanFactory
 {
-    /*public BeanFactory()
+    @Bean(value = "myds")
+    public DataSource getDataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource(Constants.URL, Constants.USER, Constants.PASSWORD);
+        ds.setDriverClassName("oracle.jdbc.OracleDriver");
+        return ds;
+    }
+    @Bean
+    @DependsOn(value = "myds")
+    public JdbcTemplate getJdbcTemplate(@Autowired @Qualifier(value = "myds") DataSource ds)
     {
-        System.err.println("----> Bean factory new yapılıyor");
-    }*/
+        return new JdbcTemplate(ds);
+    }
 
-    @Bean(name = "person1")
-    public Person getPerson()
-    {
-        return new Person(12, "şerafettin");
+    @Bean
+    @DependsOn(value = "myds")
+    public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(@Autowired @Qualifier(value = "myds") DataSource ds){
+        return new NamedParameterJdbcTemplate(ds);
+
     }
 }
